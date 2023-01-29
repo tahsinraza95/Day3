4f8419bec5811635024e193c650b4694d73e33b8
tahsinraza95 <tahsinr693@gmail.com>
2023-01-29 12:31:05 +0530

    added: Array

---
+++ Array/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] arr = new int[]{33,43,65,76,534,465};
+            for (int i=0;i<arr.Lenght;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
